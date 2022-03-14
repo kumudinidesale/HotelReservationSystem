1bde89411be6accabf23e82de58719dc154311d1
kumudinidesale <kdesale1211@gmail.com>
2022-03-14 09:00:18 +0530

    added:WellCome to HotelReaservation System

---
+++ src/main/java/com/java/hotelreaservationsystem/HotelReaservationSystem.java
@@ -0,0 +1,8 @@
+package com.java.hotelreaservationsystem;
+
+public class HotelReaservationSystem {
+	public static void main(String[] args) {
+		System.out.println("WellCome to HotelReservation System");
+	}
+
+}
